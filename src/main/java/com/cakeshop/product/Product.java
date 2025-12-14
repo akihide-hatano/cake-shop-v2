4f4821bb33fe59b443fb0523b14aa3545884d8b2
akihide-hatano <a.h.19841202@gmail.com>
2025-12-14 18:07:52 +0900

    Productクラスの修正

--- src/main/java/com/cakeshop/product/Product.java
+++ src/main/java/com/cakeshop/product/Product.java
@@ -8,6 +8,7 @@ public abstract class Product {
     private final int basePrice;
     private final PricePolicy pricePolicy;
 
+    //インスタスの作成
     protected Product(String name,String description,int basePrice,PricePolicy pricePolicy){
         this.name = name;
         this.description = description;
@@ -15,5 +16,23 @@ public abstract class Product {
         this.pricePolicy = pricePolicy;
     }
 
-    
+    public String gatName(){
+        return name;
+    }
+
+    public String git push -u origin main(){
+        return description;
+    }
+
+    public int gatPrice(){
+        return pricePolicy.calculate(basePrice)
+    }
+
+    //継承先で必ずカテゴリを決めさせる
+    public abstract String getCategory();
+
+    // 表示用（オーバーライド可）
+    public String label() {
+        return String.format("[%s] %s - %d円", getCategory(), name, getPrice());
+    }
 }
